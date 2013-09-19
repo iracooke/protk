9050704c82eb2664f1e06787fd4a37e415ace747
Ira Cooke <iracooke@gmail.com>
2013-09-19 13:55:42 +1000

    Aligner passing some simple tests. Aligns peptides to nucleotide sequences for proteogenomics applications

--- lib/protk/gapped_aligner.rb
+++ lib/protk/gapped_aligner.rb
@@ -7,29 +7,90 @@ class PeptideFragment
 	attr_accessor :seq
 end
 
+class PeptideToGeneAlignment
+	attr_accessor :gene_seq
+	attr_accessor :pep_seq
+	attr_accessor :trace
+
+	def initialize(gene,peptide,trace)
+		@gene_seq = gene
+		@pep_seq = peptide
+		@trace = trace
+	end
+
+	def inspect
+		pep_triples=""
+		@pep_seq.each_char { |c| pep_triples<<c;pep_triples<<c;pep_triples<<c }
+		gene_seq_triples=""
+		Bio::Sequence::NA.new(@gene_seq).translate.each_char do |c| 
+			gene_seq_triples<<c;gene_seq_triples<<c;gene_seq_triples<<c 
+		end
+
+		descr = "#{@gene_seq}\n#{gene_seq_triples}\n"
+		pepi=0
+		@trace.each_with_index do |move, i|  
+			if move==1
+				descr<<"-"
+			elsif move==0
+				descr<<"#{pep_triples[pepi]}"
+				pepi+=1
+			end
+		end
+		descr<<"\n"
+		puts descr
+	end
+
+	def gaps
+		gps=[]
+		in_start_end=true
+		in_gap=false
+		@trace.each_with_index do |move, i| 
+			if move==0
+				in_start_end=false
+				if in_gap #Ending a gap
+					gps.last[1]=i
+				end
+				in_gap=false
+			else
+				if !in_start_end && !in_gap #Starting a gap
+					in_gap=true
+					gps<<[i,0]
+				end
+			end				
+		end
+		#Remove gaps that have zero length (Trailing)
+		gps=gps.collect do |gp| 
+			rv=gp
+			if gp[1]==0
+				rv=nil
+			end		
+			rv
+		end
+		gps.compact!
+		gps
+	end
+
+end
+
 class GappedAligner
-
-	
-
-	@big_penalty
-	@gap_open_penalty
-	@gap_extend_penalty
 
 	def initialize
 		@big_penalty = -100000
 		@gap_open_penalty = -10
 		@gap_extend_penalty = -1
 		@end_gap_penalty = 0
+
 		@match_move=0
 		@aadel_move=-1
 		@nadel_move=1
+		@triplet_offsets = [[0,-2,-1],[-1,0,-2],[-2,-1,0]]
 	end
 
 	def aa_deletion()
 		return @big_penalty
 	end
 
-	def na_deletion(move_type)
+	def score_na_deletion(move_type)
 		if move_type==@nadel_move
 			return @gap_extend_penalty
 		end
@@ -52,31 +113,43 @@ class GappedAligner
 		elsif last_move==@nadel_move			
 			last_row+=1
 		end
-#		puts "#{last_row},#{last_col}"
+
 		if last_col==0 && last_row==0
 			return [last_move]
 		else
-#			require 'debugger';debugger
-
 			throw "Beyond end of array" if last_col<0 || last_row <0
 
 			return traceback(last_row,last_col,dpmoves).push(last_move)
 		end
-#		result
 	end
 
+	def next_frame(previous_frame)
+		(previous_frame+1) % 3
+	end
+
+	def translate_na_at(j,frame,gene_seq)
+		rm = j % 3 
+		start_pos=j+@triplet_offsets[rm][frame]
+		if start_pos < 0
+			return '-'
+		else
+			return gene_seq[start_pos,3].translate
+		end
+	end
 
 	def calculate_dp(pep_seq,gene_seq)
-		nrow = pep_seq.length+1
+		gene_seq = Bio::Sequence::NA.new(gene_seq)
+		nrow = pep_seq.length*3+1
 		ncol = gene_seq.length+1
 
 		throw "Peptide sequence is longer than gene" if nrow > ncol
 
+		pep_triples=""
+		pep_seq.each_char { |c| pep_triples<<c;pep_triples<<c;pep_triples<<c }
 
-		dpmoves=Matrix.build(nrow,ncol) {|r,c| 0 }
-		dpmatrix=Matrix.build(nrow,ncol) { |r,c| 0 }
-		dpmatrix = dpmatrix.to_a
-		dpmoves = dpmoves.to_a
+		dpmoves=Matrix.build(nrow,ncol) {|r,c| 0 }.to_a
+		dpmatrix=Matrix.build(nrow,ncol) { |r,c| 0 }.to_a
+		dpframes=Matrix.build(nrow,ncol) { |r,c| 0 }.to_a
 
 		# Boundary conditions
 		(0..(nrow-1)).each { |i| 
@@ -91,37 +164,39 @@ class GappedAligner
 
 		(1..(nrow-1)).each do |i|
 			(1..(ncol-1)).each do |j|
-				aa = pep_seq[i-1]
-				na = gene_seq[j-1]
+				aa = pep_triples[i-1]
 
-				match = score_match(aa,na) + dpmatrix[i-1][j-1]
+				translated_na = translate_na_at(j-1,dpframes[i-1][j-1],gene_seq)
 
-				nadel = na_deletion(dpmoves[i][j-1]) + dpmatrix[i][j-1]
+				match = score_match(aa,translated_na) + dpmatrix[i-1][j-1]
+
+				nadel = score_na_deletion(dpmoves[i][j-1]) + dpmatrix[i][j-1]
 
 				if match >= nadel
 					dpmatrix[i][j] = match					
 					dpmoves[i][j] = @match_move
+					dpframes[i][j] = dpframes[i-1][j-1]
 				else
 					dpmatrix[i][j] = nadel
 					dpmoves[i][j] = @nadel_move
+					dpframes[i][j] = next_frame(dpframes[i][j-1])
 				end
-
 			end
 		end
-		require 'debugger';debugger
+
+#		require 'debugger';debugger
 		traceback(nrow-1,ncol-1,dpmoves)
 	end
 
 
 	def align pep_seq, gene_seq
-		# Against "ABCDEFGHI"
-		pep_seq="CDE"
-		pep_seq="CFGH" #Expect [1,1,0,1,1,0,0,0,1]
 
 		trace = calculate_dp(pep_seq,gene_seq)
-		require 'debugger';debugger
+		alignment = PeptideToGeneAlignment.new(gene_seq,pep_seq,trace)
+		# puts alignment
+		# require 'debugger';debugger
 
-		return [PeptideFragment.new(),PeptideFragment.new()]
+		return alignment
 	end
 
 end
--- spec/gapped_aligner_spec.rb
+++ spec/gapped_aligner_spec.rb
@@ -16,12 +16,35 @@ describe GappedAligner do
 		@aligner.respond_to?(:align).should be_true
 	end
 
-	it "should align over a gap" do
-		reference="ABCDEFGHI"
-		subject="ABFG"
+	it "should align with no gaps" do
+		reference="ggtttttgcagg"
+		# Translation is GFCR
+		subject="FC"
 		alignment = @aligner.align(subject,reference)
-		alignment[0].class.should eql PeptideFragment
+		alignment.class.should eql PeptideToGeneAlignment
+		alignment.trace.should eql [1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1]
+		alignment.gaps.should eql []
 	end
 
+
+	it "should align over a gap" do
+		reference="ggtttttgcaggagg"
+		# Translation is GFCRR
+		subject="FR"
+		alignment = @aligner.align(subject,reference)
+		alignment.class.should eql PeptideToGeneAlignment
+		alignment.trace.should eql [1, 1, 1, 0, 0, 0, 1,1,1, 1, 1, 1, 0, 0, 0]
+		alignment.gaps.should eql [[6,12]]
+	end
+
+	it "should align over a gap including a frameshift" do
+		reference="ggtttttcaggagg"
+		# Translation is GF*RR
+		subject="FR"
+		alignment = @aligner.align(subject,reference)
+		alignment.class.should eql PeptideToGeneAlignment
+		alignment.trace.should eql [1, 1, 1, 0, 0, 0, 1,1,1, 1, 1,  0, 0, 0]
+		alignment.gaps.should eql [[6,11]]
+	end
 
 end
