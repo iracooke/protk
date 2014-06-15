bbb91b8515f46438d6164598edc2870bb6e731f1
Ira Cooke <iracooke@gmail.com>
2014-06-15 20:34:38 +1000

    Created shared examples for commandline tools

---
+++ spec/commandline_shared_examples.rb
@@ -0,0 +1,18 @@
+
+
+RSpec.shared_examples "a protk tool" do 
+
+	before(:each) do
+		@tool_name=subject[0]
+	end
+
+	it "should be installed" do
+		output=%x[which #{@tool_name}]
+		expect(output).to match(/#{@tool_name}$/)
+	end
+
+	it "should print help text if no arguments are given" do
+		output=%x[#{@tool_name}]
+		expect(output).to match("Usage: #{@tool_name}")
+	end
+end
