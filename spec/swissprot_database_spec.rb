e7aba43c4f467231ad159d08c4760ccc81b19609
Ira Cooke <iracooke@gmail.com>
2014-08-28 16:44:52 +1000

    Refactor tests for swissprot_database

--- spec/swissprot_database_spec.rb
+++ spec/swissprot_database_spec.rb
@@ -16,21 +16,39 @@ describe SwissprotDatabase do
     expect(spdatabase).to be_a SwissprotDatabase
 	end
 	
-	
-	it "should be possible to find a single entry for ITAM_HUMAN" do
-    itam=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(itam).to be_a Bio::SPTR
-	end
+  let(:itam_human_entry) {
+    spdatabase.get_entry_for_name('ITAM_HUMAN')
+  }
+
+  describe "ITAM_HUMAN entry" do
+    subject { itam_human_entry }
+    it { should be_a Bio::SPTR }
+    its(:recname) { should eq("Integrin alpha-M")}
+    its(:accessions) { should eq(["P11215", "Q4VAK0", "Q4VAK1", "Q4VAK2"])}
+    its(:cd) { should eq("CD11b")}
+    its(:altnames) { should match(/CD11 antigen-like family member B; CR-3 alpha chain; Cell surface /)}
+    its(:location) { should match(/Membrane; Single-pass type I membrane protein/) }
+    its(:function) { should match(/Integrin alpha-M\/beta-2 is implicated in /) }
+    its(:similarity) { should match(/Belongs to the integrin alpha chain family/)}
+    its(:disease) { should match(/lupus erythematosus/) }
+    its(:tissues) { should match(/monocytes and granulocytes/)}
+    its(:domain) { should match(/I-domain/)}
+    its(:subunit) { should match(/alpha and a beta/)}
+    its(:intact) { should eq("P11215") } 
+    its(:pride) { should eq("P11215") }
+    its(:ensembl) { should eq("ENST00000287497")}
+    its(:nextbio) { should eq("14419")}
+    its(:num_transmem) { should eq("1")}
+    its(:signalp) { should eq("1")}
+    its(:go_terms) {should include("GO:0009897") }
+    its(:go_entries) { should include(["GO:0009897", "C:external side of plasma membrane", "IEA:Ensembl"])}
+  end
 
 	it "should fail gracefully when asked to search for a non-existent protein BLAHBLAH" do
     itam=spdatabase.get_entry_for_name('BLAHBLAH')
     expect(itam).to be_nil
 	end
 	
-	it "should correctly parse the recommended name for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.recname).to eq "Integrin alpha-M"
-	end
 
   it "should be possible to access a key by name" do
     item=spdatabase.get_entry_for_name('ITAM_HUMAN')
@@ -38,98 +56,5 @@ describe SwissprotDatabase do
     rn=item.send(key)
     expect(rn).to eq("Integrin alpha-M")
   end
-
-  it "should correctly obtain the list of accessions for ITAM_HUMAN" do
-    item = spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.accessions).to eq(["P11215", "Q4VAK0", "Q4VAK1", "Q4VAK2"])
-  end
-
-	it "should correctly parse the CD Antigen name for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.cd).to eq("CD11b")
-	end
-	
-	it "should correctly parse the Alternate names for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.altnames).to match(/CD11 antigen-like family member B; CR-3 alpha chain; Cell surface glycoprotein MAC-1 subunit alpha; Leukocyte adhesion receptor MO1; Neutrophil adherence receptor/)
-	end
-	
-	it "should correctly parse the SubCellular Location field for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    # require 'debugger';debugger
-    expect(item.location).to match(/Membrane; Single-pass type I membrane protein/)
-	end
-	
-	it "should correctly parse the Function field for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.function).to match(/Integrin alpha-M\/beta-2 is implicated in /)
-	end
-	
-	it "should correctly parse the Similarity field for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.similarity).to match(/Belongs to the integrin alpha chain family/)
-	end
-	
-	it "should correctly parse the Tissue Specificity field for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.tissues).to match(/Predominantly expressed in monocytes and granulocytes/)
-	end
-
-	it "should correctly parse the Disease field for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.disease).to match(/lupus erythematosus /)
-	end
-	
-	it "should correctly parse the Domain field for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.domain).to match(/The integrin I-domain \(insert\) is a VWFA domain/)
-	end
-	
-	it "should correctly parse the Subunit field for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.subunit).to match(/Heterodimer of an alpha and a beta subunit/)
-	end
-
-	it "should correctly obtain a link to the Interaction database entry for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.intact).to eq("P11215") 
-  end
-  
-	it "should correctly obtain a link to the Pride entry for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.pride).to eq("P11215") 
-  end
-
-	it "should correctly obtain a link to the Ensembl entry for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.ensembl).to eq("ENST00000287497")
-  end
-  
-  it "should correctly obtain a link to the NextBio entry for ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.nextbio).to eq("14419")
-  end
-  
-  it "should correctly the number of transmembrane regions in ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.num_transmem).to eq("1")	  
-  end
-  
-  it "should correctly the number of signal regions in ITAM_HUMAN" do
-    item=spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.signalp).to eq("1")
-  end
-
-  it "should correctly retrieve the GO terms" do
-    item = spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.go_terms).to be_a Array
-    expect(item.go_terms[0]).to eq("GO:0009897")
-  end
-  
-  it "should correctly retrieve full GO entries" do
-    item = spdatabase.get_entry_for_name('ITAM_HUMAN')
-    expect(item.go_entries).to be_a Array
-    expect(item.go_entries[0][0]).to eq("GO:0009897")
-  end  
 	
 end
