0600dcf29d2576cf06338eb4a8fc79dffc832a9c
Ira Cooke <iracooke@gmail.com>
2015-08-10 14:47:48 +1000

    Use limxml Reader API for filter_psms

--- bin/filter_psms.rb
+++ bin/filter_psms.rb
@@ -40,53 +40,64 @@ output_fh = tool.explicit_output!=nil ? File.new("#{tool.explicit_output}",'w') 
 throw "Input file #{input_file} does not exist" unless File.exist? "#{input_file}"
 
 XML::Error.set_handler(&XML::Error::QUIET_HANDLER)
-pepxml_parser=XML::Parser.file("#{input_file}")
-pepxml_ns_prefix="xmlns:"
-pepxml_ns="xmlns:http://regis-web.systemsbiology.net/pepXML"
 
-$protk.log "Parsing #{input_file}" , :info
+doc = XML::Document.file("#{input_file}")
+reader = XML::Reader.document(doc)
 
-pepxml_doc=pepxml_parser.parse
-unless pepxml_doc.root.namespaces.default
-  pepxml_ns_prefix=""
-  pepxml_ns=nil
+
+# First print out the header (ie before spectrum_queries)
+File.foreach("#{input_file}") do |line|  
+  if line =~ /\<spectrum_query/
+    break;
+  else
+    output_fh.write line
+  end
 end
 
-$protk.log "Finding search hits" , :info
-
-search_hits=pepxml_doc.find("//#{pepxml_ns_prefix}search_hit[@hit_rank=\"1\"]", pepxml_ns)
+pepxml_ns_prefix="xmlns:"
+pepxml_ns="xmlns:http://regis-web.systemsbiology.net/pepXML"
 
 kept=0
 deleted=0
 scanned=0
-$protk.log "Filtering #{search_hits.length} hits" , :info
 
-search_hits.each do |hit|
+while reader.read
 
-  has_match = expressions.collect { |expression|   (hit.attributes[tool.filter] =~ /#{expression}/) }.any?
+  if reader.name == "spectrum_query"
+    sq_node = reader.expand
 
-  if !has_match && tool.check_alternative_proteins
-    alts = hit.find("./#{pepxml_ns_prefix}alternative_protein",pepxml_ns)
+    hits = sq_node.find("./#{pepxml_ns_prefix}search_result/#{pepxml_ns_prefix}search_hit[@hit_rank=\"1\"]",pepxml_ns)
 
-    # Check alternative proteins
-    alt_expr = alts.collect { |alt| expressions.collect { |expression| (alt.attributes[tool.filter] =~ /#{expression}/ )}}
+    throw "More than one first ranked search hit" if hits.length>1
+    throw "No search hit for spectrum_query" if hits.length==0
 
-    has_match = alt_expr.flatten.any?
+    hit = hits[0]
+
+    has_match = expressions.collect { |expression|   (hit.attributes[tool.filter] =~ /#{expression}/) }.any?
+
+    if !has_match && tool.check_alternative_proteins
+      alts = hit.find("./#{pepxml_ns_prefix}alternative_protein",pepxml_ns)
+
+      # Check alternative proteins
+      alt_expr = alts.collect { |alt| expressions.collect { |expression| (alt.attributes[tool.filter] =~ /#{expression}/ )}}
+
+      has_match = alt_expr.flatten.any?
+    end
+
+    if (has_match && !tool.reject_mode) || (!has_match && tool.reject_mode)  #&& (hit.attributes['hit_rank']=="1")
+      kept+=1
+      output_fh.write "#{sq_node}\n"
+    else
+      deleted+=1
+    end
+
+    scanned+=1
+
+    reader.next_sibling
   end
 
-  if (has_match && !tool.reject_mode) || (!has_match && tool.reject_mode)  #&& (hit.attributes['hit_rank']=="1")
-    kept+=1
-  else
-    hit.parent.parent.remove!
-    deleted+=1
-  end
-
-  scanned+=1
-  if (scanned % 1000 == 0) && tool.debug
-    $stdout.write "Kept #{kept} and deleted #{deleted}\n"
-  end
 end
 
-output_fh.write pepxml_doc
+output_fh.write "</msms_run_summary>\n</msms_pipeline_analysis>\n"
 
 $protk.log "Kept #{kept} and deleted #{deleted}" , :info
